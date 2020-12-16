7bc3bc3eb41d0f4df74a05a0fab79ae965e486be
BuildTools <unconfigured@null.spigotmc.org>
2020-12-16 14:02:16 +0100

    Replaced Mongo with Nitrite

--- src/main/java/com/wipdev/tutorbot/database/DatabaseHandler.java
+++ src/main/java/com/wipdev/tutorbot/database/DatabaseHandler.java
@@ -1,58 +1,54 @@
 package com.wipdev.tutorbot.database;
 
-import com.mongodb.*;
-import com.mongodb.client.MongoCursor;
-import org.bson.Document;
-import org.bson.types.ObjectId;
+
+import org.dizitart.no2.Cursor;
+import org.dizitart.no2.Document;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.internals.JacksonMapper;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DatabaseHandler {
 
 
-    private MongoClientURI uri;
-
-    private MongoClient mongoClient;
+    Nitrite db;
 
     public final String userKey = "user", passwordKey = "password";
     public final String idKey = "_id";
 
     public void connect() {
-        uri = new MongoClientURI("mongodb+srv://Admin:2212@cluster0.jquix.mongodb.net/user_data?retryWrites=true&w=majority");
+        db = Nitrite.builder().compressed().filePath("res/data.db").openOrCreate("admin","2212");
 
-        mongoClient = new MongoClient(uri);
     }
 
     private void insertOne(Database database, JSONObject object) {
-        mongoClient.getDatabase(database.databaseName).getCollection(database.collectionName).insertOne(Document.parse(object.toString()));
+        db.getCollection(database.databaseName).insert(new Document(object.toMap()) );
     }
 
     private JSONObject find(Database database, String key, Object value) {
-        MongoCursor<Document> it = mongoClient.getDatabase(database.databaseName).getCollection(database.collectionName).find().iterator();
+        Iterator<Document> it= db.getCollection(database.databaseName).find().iterator();
         while (it.hasNext()) {
             var val = it.next();
             if (!val.containsKey(key)) continue;
-            if (val.getString(key).equals(value)) {
-                var ret = new JSONObject(val.toJson());
-                it.close();
+            if (((String)(val.get(key))).equals(value)) {
+                JacksonMapper mapper = new JacksonMapper();
+                var ret = new JSONObject( mapper.toJson(mapper.asDocument(val)));
                 return ret;
             }
         }
-        it.close();
         return null;
     }
 
     private List<JSONObject> getAll(Database database){
         List<JSONObject> objects = new ArrayList<>();
-        MongoCursor<Document> it = mongoClient.getDatabase(database.databaseName).getCollection(database.collectionName).find().iterator();
-        while (it.hasNext()) {
-            var val = it.next();
-            var ret = new JSONObject(val.toJson());
-            objects.add(ret);
+        Cursor cursor = db.getCollection(database.databaseName).find();
+        JacksonMapper mapper = new JacksonMapper();
+        for(Document doc  : cursor){
+            objects.add(new JSONObject(mapper.toJson(mapper.asDocument(doc))));
         }
-        it.close();
         return objects;
     }
 
